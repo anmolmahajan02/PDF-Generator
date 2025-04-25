edc00dcd00bf5fb0304bd5498eeeac08cb04207a
anmolmahajan02 <anmolmahajan236@gmail.com>
2025-04-26 03:42:06 +0530

    Download button not working

--- pdf/views.py
+++ pdf/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from urllib.parse import urlencode
+from django.template.loader import render_to_string
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def degree(request):
-    return render(request,"degree.html")
+    if request.method == "POST":
+        name = request.POST.get('name') 
+        uniName = request.POST.get('uniName') 
+        degreeName = request.POST.get('degree') 
+        cgpa = request.POST.get('cgpa') 
+
+        query_string = urlencode({
+            'name':name,
+            'uniName':uniName,
+            'degree':degreeName,
+            'cgpa':cgpa})
+        degreepdf_url = reverse('degreepdf')
+        return redirect(f"{degreepdf_url}?{query_string}")
+    else:
+        return render(request,"degree.html")
 
 def certificate(request):
     return render(request,"certificate.html")
@@ -14,4 +32,14 @@ def list(request):
     return render(request,'list.html')
 
 def degreepdf(request):
-    return render(request,'degreepdf.html')
+    name = request.GET.get('name') 
+    uniName = request.GET.get('uniName') 
+    degreeName = request.GET.get('degree') 
+    cgpa = request.GET.get('cgpa')
+    
+    return render(request, "degreepdf.html", {
+        'name': name,
+        'uniName': uniName,
+        'degree': degreeName,
+        'cgpa': cgpa
+    })
